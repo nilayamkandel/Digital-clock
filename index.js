3179a7b37e2780451d838c97d0b5fe43fa873a6a
neelayam kandel <neelayamdottkandel@gmail.com>
2023-05-23 16:57:19 +0545

    docs: comment added

--- index.js
+++ index.js
@@ -9,16 +9,22 @@ const ampmEl = document.getElementById("ampm")
 function updateClock(){
 
     // creating  variable
+   // here Date().getHours() returns the hour in the specified date according to local time.
+
     let h = new Date().getHours()
     let m = new Date().getMinutes()
     let s = new Date().getSeconds()
     let ampm = "AM"
+
+    // now we are using if to change am and pm.
 
     if(h > 12){
         h = h - 12
         ampm= "PM"
     }
     // this means if the number is less than 10 use 01,02.. otherwise dont use
+    // the syntax of tertiary operator is (condition) ? (return if true) : (return if false);
+    // here (h < 10) is condition, ("0" + h) is true and h is false
     h = h < 10 ? "0" + h : h;
     m = m < 10 ? "0" + m : m;
     s = s < 10 ? "0" + s : s;
@@ -30,9 +36,10 @@ function updateClock(){
     secondEl.innerText = s;
     ampmEl.innerText = ampm;
     // after every one second the function is called again. its like an infinite loop
-    setTimeout(()=>{
-        updateClock()
-    },1000)
+    // The setTimeout() method calls a function after a number of milliseconds. 1 second = 1000 milliseconds.
+    // The global setTimeout() method sets a timer which executes a function once the timer expires.
+
+    setTimeout(()=>{updateClock()},1000)
 
 }
     updateClock()
